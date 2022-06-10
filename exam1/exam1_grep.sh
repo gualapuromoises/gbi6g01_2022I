466bfd79b99757a478cd9a064b940be9fd9d61f9
josand00 <josuemart200013@gmail.com>
2022-06-10 15:06:14 -0500

    cambio4

--- exam1/exam1_grep.sh
+++ exam1/exam1_grep.sh
@@ -1,4 +1,5 @@
-#! bin/ 
+#! bin/bash  
 
  mkdir grep ; 
-grep -i mus musculus ../data/interleukin.txt  >> mouse.txt
+grep -i "mus musculus" ../data/interleukin.txt  > grep/mouse.txt ; grep -i "homo sapiens" ../data/interleukin.txt > grep/human.txt  ; grep -i "linear mRNA" ../data/interleukin.txt  > grep/linear_rna.txt
+
