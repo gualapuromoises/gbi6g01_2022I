45ddea48ff70be9e27501365fe14f0aa42abbc49
Valekia <angiely.camacho@est.ikiam.edu.ec>
2022-06-10 14:55:49 -0500

    cambio2

--- exam1/exam1_grep.sh
+++ exam1/exam1_grep.sh
@@ -2,5 +2,5 @@
 mkdir grep
 grep "Bos taurus" ../data/interleukin.txt >> grep/cattle.txt;
 
-grep "Gallus gallus" ../data/interleukin.txt >> grep/chicken.txt
-
+grep "Gallus gallus" ../data/interleukin.txt >> grep/chicken.txt;
+grep "linear DNA" ../data/interleukin.txt >> grep/linear_dna.txt
